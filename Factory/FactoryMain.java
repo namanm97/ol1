2ce40ed055b5ea2d85ec9cb26e3fe56f145c9e2a
Ansh Arora <ansharora2299@gmail.com>
2018-04-29 22:59:09 +0530

    initial commit

---
+++ Factory/FactoryMain.java
@@ -0,0 +1,13 @@
+package pkgFactory;
+
+public class FactoryMain {
+    public static void main(String args[])
+    {
+        Customer c;
+        CategoryFactory cf=new CategoryFactory();
+        c=cf.category("Teen");
+        
+        c=cf.category("kidds");
+        
+    }
+}
